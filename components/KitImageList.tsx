08600db15b09c7b33b64f3b6aad1388d55ba7384
Kyleasmth <45497561+Kyleasmth@users.noreply.github.com>
2023-02-10 12:49:21 -0800

    clean up KitImageList

--- components/KitImageList.tsx
+++ components/KitImageList.tsx
@@ -5,6 +5,8 @@ import ChevronRight from "@mui/icons-material/ChevronRight";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Image from "next/image";
+import kitsTheme from "@/styles/kitsTheme";
+import { ThemeProvider } from "@mui/material/styles";
 
 interface KitImageListProps {
   kitData: KitItemType[];
@@ -27,68 +29,73 @@ const KitImageList: React.FC<KitImageListProps> = ({
   index,
 }) => {
   return (
-    <Box sx={{ display: "flex " }}>
-      <ChevronLeft
-        sx={{
-          width: "30px",
-          height: "30px",
-          borderRadius: "50%",
-          color: "grey",
-          backgroundColor: "white",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          marginRight: "10px",
-          marginLeft: "10px",
-          cursor: "pointer",
-        }}
-        onClick={() =>
-          index - 1 < 0 ? setIndex(kitData.length - 1) : setIndex(index - 1)
-        }
-      />
-      <ImageList
-        sx={{
-          width: "400px",
-          gridAutoFlow: "column",
-          gridTemplateColumns: "repeat(auto-fit, minmax(70px,1fr)) !important",
-          gridAutoColumns: "minmax(70px, 1fr)",
-          overflow: "hidden",
-        }}
-      >
-        {kitData.map((kit, i) => {
-          const styles =
-            i === index
-              ? { borderColor: "green", border: "2px solid green" }
-              : { borderColor: "white", border: "1px solid white" };
-          return (
-            <div key={i} onClick={() => setIndex(i)}>
-              <ImageListItem sx={styles} key={i}>
-                <Image src={kit.src} alt={kit.imgAlt} width={66} height={50} />
-              </ImageListItem>
-            </div>
-          );
-        })}
-      </ImageList>
-      <ChevronRight
-        sx={{
-          width: "30px",
-          height: "30px",
-          borderRadius: "50%",
-          borderBlockColor: "grey",
-          color: "grey",
-          backgroundColor: "white",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          marginRight: "10px",
-          marginLeft: "10px",
-          cursor: "pointer",
-        }}
-        onClick={() => {
-          index + 1 > kitData.length - 1 ? setIndex(0) : setIndex(index + 1);
-        }}
-      />
-    </Box>
+    <ThemeProvider theme={kitsTheme}>
+      <Box sx={{ display: "flex " }}>
+        <ChevronLeft
+          sx={{
+            width: "30px",
+            height: "30px",
+            borderRadius: "50%",
+            color: "grey",
+            backgroundColor: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: "10px",
+            marginLeft: "10px",
+            cursor: "pointer",
+          }}
+          onClick={() =>
+            index - 1 < 0 ? setIndex(kitData.length - 1) : setIndex(index - 1)
+          }
+        />
+        <ImageList
+          sx={{
+            width: "400px",
+            gridAutoFlow: "column",
+            overflow: "hidden",
+          }}
+        >
+          {kitData.map((kit, i) => {
+            const styles =
+              i === index
+                ? { border: `1.5px solid ${kitsTheme.palette.primary.main}` }
+                : null;
+            return (
+              <div key={i} onClick={() => setIndex(i)}>
+                <ImageListItem sx={styles} key={i}>
+                  <Image
+                    src={kit.src}
+                    alt={kit.imgAlt}
+                    width={67}
+                    height={50}
+                  />
+                </ImageListItem>
+              </div>
+            );
+          })}
+        </ImageList>
+        <ChevronRight
+          sx={{
+            width: "30px",
+            height: "30px",
+            borderRadius: "50%",
+            borderBlockColor: "grey",
+            color: "grey",
+            backgroundColor: "white",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: "10px",
+            marginLeft: "10px",
+            cursor: "pointer",
+          }}
+          onClick={() => {
+            index + 1 > kitData.length - 1 ? setIndex(0) : setIndex(index + 1);
+          }}
+        />
+      </Box>
+    </ThemeProvider>
   );
 };
 
