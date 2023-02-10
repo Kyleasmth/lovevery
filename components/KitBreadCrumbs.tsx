f489d14e872f96db6cced7b148df5a673c0c4af2
Kyleasmth <45497561+Kyleasmth@users.noreply.github.com>
2023-02-10 12:37:56 -0800

    update styling to breadcrumbs

--- components/KitBreadCrumbs.tsx
+++ components/KitBreadCrumbs.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 interface BreadCrumbProps {
   kitTitle: string;
@@ -12,13 +13,19 @@ const KitBreadCrumbs: React.FC<BreadCrumbProps> = ({ kitTitle }) => {
     <Box sx={{ flex: 1, padding: 3 }}>
       <Breadcrumbs aria-label="breadcrumb">
         <Link color="inherit" href="/">
-          Home
+          <Typography color="common.black" fontWeight="bold">
+            Home
+          </Typography>
         </Link>
         <Link color="inherit" href="/">
-          Play Kits
+          <Typography color="common.black" fontWeight="bold">
+            Play Kits
+          </Typography>
         </Link>
         <Link color="inherit" href="/">
-          {kitTitle}
+          <Typography color="textSecondary" fontWeight="bold">
+            {kitTitle}
+          </Typography>
         </Link>
       </Breadcrumbs>
     </Box>
